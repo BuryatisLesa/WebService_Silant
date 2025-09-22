7b02159ad948eb8001a0e399e37d8115f81cca7d
Konstantin Sandanov <kostyansandanov@gmail.com>
2025-09-23 04:20:43 +0800

    feat: добавлена первичные CRUD для модели Machine

---
+++ servicing/service/views/machine.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
+from rest_framework import status
+
+from service.models.machine import Machine
+
+from service.serializers import (
+    MachineSerializer,
+    )
+
+@api_view(["GET"])
+def machine_list(request):
+    machines = Machine.objects.all()
+    serializer = MachineSerializer(machines, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def machine_detail(request, id):
+    machine = get_object_or_404(Machine, id=id)
+    serializer = MachineSerializer(machine)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def machine_create(request):
+    serializer = MachineSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["PUT"])
+def machine_update(request, id):
+    machine = get_object_or_404(Machine, id=id)
+    serializer = MachineSerializer(instance=machine, data=request.data, partial=True)  
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["DELETE"])
+def machine_delete(request, id):
+    machine = get_object_or_404(Machine, id=id)
+    machine.delete()
+    return Response({"message": "Machine deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
